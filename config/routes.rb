6b2e82359ad11a4d9b7ee6b93e02d24eff3d1c88
Daniel <danieljmatney@gmail.com>
2020-03-15 18:23:32 -0500

    Added nested route for a users gearsets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :gearsets, :pieces
+  resources :gearsets
+  resources :pieces only: [:index, :show]
+  resources :users, only: [:show] do
+    resources :gearsets, only: [:show, :index]
+  end
 end
